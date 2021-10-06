dede8bb5c3d94116d0c634ca1cdfbd1e28f415f5
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-05 23:15:17 -0500

    [ADD] - Methods to class added

--- classObj/Cup.py
+++ classObj/Cup.py
@@ -1,11 +1,15 @@
 """Class/ object Cup"""
 
 
+from _typeshed import Self
+
+
 class Cup:
     """Constructor"""
     def __init__(self, name="", count_teams=0):
         self.__name = name
         self.__count_teams = count_teams
+        self.__teams = None
 
     """Get name"""
     def get_name(self):
@@ -23,5 +27,23 @@ class Cup:
     def set_count_teams(self, value):
         self.__count_teams = value
 
+    """Get teams"""
+    def get_teams(self):
+        return self.__teams
+
+    """Set team to teams"""
+    def set_teams(self, value):
+        self.__teams.append(value)
+
     name = property(get_name, set_name)
     count_teams = property(get_count_teams, set_count_teams)
+    teams = property(get_teams, set_teams)
+
+    def create_groups(list=[]):
+        """Code here"""
+
+    def compare_teams(pot, new_team):
+        """Code here"""
+
+    def next_round(list=[]):
+        """Code here"""
