15c24fe93b5f90482d0d22460f90c9390027dd07
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-12-31 13:48:51 -0500

    [UPD] - change in create_groups to acount of group to create, clean_Reasult and shuffle added

--- classObj/Cup.py
+++ classObj/Cup.py
@@ -1,6 +1,6 @@
 """Class/ object Cup"""
 
-
+import random
 from typing import List
 from classObj.Group import Group
 
@@ -38,8 +38,9 @@ class Cup:
     name = property(get_name, set_name)
     teams = property(get_teams, set_teams)
 
-    def create_groups(self, list=[[]]):
-        for i in range(0, 8):
+    def create_groups(self, list=[[]], count=8):
+        self.__groups = []
+        for i in range(0, count):
             group = Group()
             self.__groups.append(group)
         for pot in list:
@@ -49,8 +50,10 @@ class Cup:
                 index += 1
         return self.__groups
 
-    def compare_teams(self, pot, new_team):
-        """Code here"""
+    def compare_teams(self, team, new_team):
+        if team.league.name == new_team.league.name:
+            return False
+        return True
 
     def next_round(self, list=[]):
         """Code here"""
@@ -70,8 +73,19 @@ class Cup:
         self.__teams = []
         self.__count_teams = 0
 
-    def shuffle(self, pot_1, pot_2):
-        pass
+    def clean_result(self):
+        print("entro")
+        for team in self.__teams:
+            # print("1---")
+            # print(team.points)
+            team.points = 0
+            # team.__wins = 0
+            # team.__lose = 0
+            # team.__tie = 0
+            # print(team.points)
+            # print("2---")
+
+    def shuffle(self, pot_head, pot_n_head):        
         """
         Logic to select random vs between teams head and NO head
         1)random to get position in index 
@@ -82,3 +96,35 @@ class Cup:
             [0, 5]-> first position(0) is position of team in POT head
             [0, 5]-> second position(5) is position of team in POT no head
         """
+        octavos = []
+        j = 0
+        while j < len(pot_head.teams):
+            flag = False
+        # for j in range(0, len(pot_head.teams)):
+            # print("INIT")
+            rnd = 0
+            vs = []
+            rnd = random.randint(0, 7)
+            size = len(octavos)
+            for i in range(0,size):
+                # print("Init i")
+                if pot_n_head.teams[rnd] in octavos[i][1:]:
+                    # print("if RND")
+                    # print("--{}--".format(j))
+                    # print("{} ---- {}".format(octavos, rnd))
+                    flag = True
+                    break
+            # print("if flag")
+            if flag is False:
+                # print("if compare teams")
+                if self.compare_teams(pot_head.teams[j], pot_n_head.teams[rnd]):
+                    # print("added in array")
+                    vs.append(pot_head.teams[j])    
+                    vs.append(pot_n_head.teams[rnd])
+                    octavos.append(vs)
+                    j += 1
+
+        return octavos
+
+            # if rnd in octavos
+            # print(rnd)     
